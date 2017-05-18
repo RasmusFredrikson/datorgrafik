5176fabdb74a950c139fb76e277811b358db5456
Rasmus Fredrikson <rasmuf@kth.se>
2017-05-18 10:04:26 +0200

    Starting lab2

---
+++ CgLab1/part2.cpp
@@ -0,0 +1,51 @@
+#include "SDL.h"
+#include <iostream>
+#include <glm/glm.hpp>
+#include <vector>
+#include "SDLauxiliary.h"
+using namespace std;
+using glm::vec3;
+// ---------------------------------------------------------
+// GLOBAL VARIABLES
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+SDL_Surface* screen;
+vector<vec3> stars(1000);
+// ---------------------------------------------------------
+// FUNCTION DECLARATIONS
+void Draw();
+// ---------------------------------------------------------
+// FUNCTION DEFINITIONS
+int main( int argc, char* argv[] )
+{
+    screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
+    for (int i = 0; i < stars.size(); ++i) {
+        float r = -1 + float(rand()%(1+1+1)) / float(RAND_MAX);
+        stars[i].x = r;
+        r = -1 + float(rand()%(1+1+1)) / float(RAND_MAX);
+        stars[i].y = r;
+        stars[i].z = float(rand()) / float(RAND_MAX);
+    }
+    while( NoQuitMessageSDL() )
+    {
+        Draw();
+    }
+    SDL_SaveBMP( screen, "screenshot.bmp" );
+    return 0;
+}
+void Draw()
+{
+    if( SDL_MUSTLOCK(screen) )
+        SDL_LockSurface(screen);
+    for( int y=0; y<SCREEN_HEIGHT; ++y )
+    {
+        for( int x=0; x<SCREEN_WIDTH; ++x )
+        {
+            vec3 color(0,0,1);
+            PutPixelSDL( screen, x, y, color );
+        }
+    }
+    if( SDL_MUSTLOCK(screen) )
+        SDL_UnlockSurface(screen);
+    SDL_UpdateRect( screen, 0, 0, 0, 0 );
+}
